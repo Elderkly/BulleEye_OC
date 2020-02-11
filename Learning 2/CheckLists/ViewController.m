29dfdc889e56d5824bdb13b0e4a763f6abd4f86b
Elderkly <897676943@qq.com>
2020-02-11 16:04:13 +0800

    no message

--- Learning 2/CheckLists/ViewController.m
+++ Learning 2/CheckLists/ViewController.m
@@ -20,11 +20,33 @@
 }
 
 - (NSInteger) tableView:(UITableView *) tableView numberOfRowsInSection:(NSInteger)section{
-    return 10;
+    return 100;
 }
 
 - (UITableViewCell *) tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
     UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
+    UILabel *label = (UILabel *)[cell viewWithTag:1000];
+    if (indexPath.row % 5 == 0) {
+        label.text = @"观看嫦娥奔月视频";
+    } else if (indexPath.row % 5 == 1) {
+        label.text = @"了解Sony a7价格";
+    } else if (indexPath.row % 5 == 2) {
+        label.text = @"复习苍老师视频";
+    } else if (indexPath.row % 5 == 3) {
+        label.text = @"去电影院看电影";
+    } else if (indexPath.row % 5 == 4) {
+        label.text = @"看西甲巴萨新败的比赛回放";
+    }
     return cell;
+}
+
+- (void) tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView cellForRowAtIndexPath:indexPath];
+    if (cell.accessoryType == UITableViewCellAccessoryNone) {
+        cell.accessoryType = UITableViewCellAccessoryCheckmark;
+    } else {
+        cell.accessoryType = UITableViewCellAccessoryNone;
+    }
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
 }
 @end
