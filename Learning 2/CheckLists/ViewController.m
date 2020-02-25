eafac08c4fb388dfbae0bc49c945fa46f87850b5
QZP743 <897676943@qq.com>
2020-02-25 18:14:57 +0800

    no message

--- Learning 2/CheckLists/ViewController.m
+++ Learning 2/CheckLists/ViewController.m
@@ -8,8 +8,13 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
-
+@interface ViewController() {
+    NSString *_row0text;
+    NSString *_row1text;
+    NSString *_row2text;
+    NSString *_row3text;
+    NSString *_row4text;
+}
 @end
 
 @implementation ViewController
@@ -17,6 +22,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    _row0text = @"观看嫦娥奔月视频";
+    _row1text = @"了解Sony a7价格";
+    _row2text = @"复习苍老师视频";
+    _row3text = @"去电影院看电影";
+    _row4text = @"看西甲巴萨新败的比赛回放";
 }
 
 - (NSInteger) tableView:(UITableView *) tableView numberOfRowsInSection:(NSInteger)section{
@@ -27,15 +37,15 @@
     UITableViewCell *cell =[tableView dequeueReusableCellWithIdentifier:@"ChecklistItem"];
     UILabel *label = (UILabel *)[cell viewWithTag:1000];
     if (indexPath.row % 5 == 0) {
-        label.text = @"观看嫦娥奔月视频";
+        label.text = _row0text;
     } else if (indexPath.row % 5 == 1) {
-        label.text = @"了解Sony a7价格";
+        label.text = _row1text;
     } else if (indexPath.row % 5 == 2) {
-        label.text = @"复习苍老师视频";
+        label.text = _row2text;
     } else if (indexPath.row % 5 == 3) {
-        label.text = @"去电影院看电影";
+        label.text = _row3text;
     } else if (indexPath.row % 5 == 4) {
-        label.text = @"看西甲巴萨新败的比赛回放";
+        label.text = _row4text;
     }
     return cell;
 }
